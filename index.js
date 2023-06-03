d78f728cb0e1e1edce2d903ed9bcea0b5d6f9915
MiguelValeraDVP <miguel.valera.97@gmail.com>
2023-06-03 16:21:22 -0300

    add atlas and config

--- index.js
+++ index.js
@@ -3,19 +3,42 @@ const cors = require("cors");
 const userRoutes = require("./routes/userRoutes.js");
 const mongoose = require("mongoose");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 
 app.use(express.json());
 
-mongoose
-  .connect("mongodb://localhost:27017/netflix-clone", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Db connected");
-  });
+const uri = process.env.DB_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// mongoose
+//   .connect("mongodb://localhost:27017/netflix-clone", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Db connected");
+//   });
 
 app.use("/api/user", userRoutes);
 
