51805e8ea5bb6d3507f2fe38463c2c8a54aa2416
MiguelValeraDVP <miguel.valera.97@gmail.com>
2023-06-06 10:48:48 -0300

    use mongoose to connect to db

--- index.js
+++ index.js
@@ -12,34 +12,14 @@ app.use(express.json());
 
 const uri = process.env.DB_URL;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// mongoose
-//   .connect("mongodb://localhost:27017/netflix-clone", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Db connected");
-//   });
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Db connected");
+  });
 
 app.use("/api/user", userRoutes);
 
